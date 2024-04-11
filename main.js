f9ced1162fcad717c4d6b0baaf28d57a93c64ac2
Fahim Hasan Khan <fahimhkn@gmail.com>
2024-04-11 01:31:08 -0700

    init

---
+++ main.js
@@ -0,0 +1,17 @@
+function main(){
+  console.log("Hello Slugs!")
+
+  let v1 = 17.5;
+  let v2 = 71;
+  let v3 = "UCSC";
+  let v4 = true;
+
+  console.log(v1,v2,v3,v4);
+
+  console.log(add(v1,v2));
+
+}
+
+function add(x1,x2){
+  return x1 + x2
+}
